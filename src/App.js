8d5ce7b5d095f963daaf04c82b39d97a78fec477
Martin  Frias Silva <Martin@macbook-martin.home>
2021-11-03 17:53:18 -0300

    se agrego titulo y primeras cards

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import '../src/components/header/header.css'
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Carrousel from './components/carrousel/Carrousel';
-
+import Title1 from './components/titles/Title1';
+import Card1 from './components/cards/Card1';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Navbar />
-      <Carrousel/>
+      <Carrousel />
+      <Title1 />
+      <Card1 />
       <Footer />
     </div>
   );
---
+++ src/components/cards/Card1.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { MDBCard, MDBCardImage, MDBCardBody, MDBCardTitle, MDBCardText, MDBRow, MDBCol } from 'mdb-react-ui-kit';
+
+export default function App() {
+  return (
+    <MDBRow className='row-cols-1 row-cols-md-3 g-4 p-4'>
+      <MDBCol>
+        <MDBCard className='h-100'>
+          <MDBCardImage
+            src='https://mdbcdn.b-cdn.net/img/new/standard/city/041.jpg'
+            alt='...'
+            position='top'
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Noticia 1</MDBCardTitle>
+            <MDBCardText>
+              This is a longer card with supporting text below as a natural lead-in to additional content.
+              This content is a little bit longer.
+            </MDBCardText>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+      <MDBCol>
+        <MDBCard className='h-100'>
+          <MDBCardImage
+            src='https://mdbcdn.b-cdn.net/img/new/standard/city/042.jpg'
+            alt='...'
+            position='top'
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Noticia 2</MDBCardTitle>
+            <MDBCardText>This is a short card.</MDBCardText>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+      <MDBCol>
+        <MDBCard className='h-100'>
+          <MDBCardImage
+            src='https://mdbcdn.b-cdn.net/img/new/standard/city/043.jpg'
+            alt='...'
+            position='top'
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Noticia 3</MDBCardTitle>
+            <MDBCardText>
+              This is a longer card with supporting text below as a natural lead-in to additional content.
+            </MDBCardText>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+      <MDBCol>
+      </MDBCol>
+    </MDBRow>
+  );
+}
