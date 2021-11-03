aa3dc9572ae41b24c7f3cd0102a44b4109092db2
Martin  Frias Silva <Martin@macbook-martin.home>
2021-11-03 17:37:09 -0300

    se creo carrousel

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Header from "./components/header/Header";
 import '../src/components/header/header.css'
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import Carrousel from './components/carrousel/Carrousel';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Navbar />
+      <Carrousel/>
       <Footer />
     </div>
   );
