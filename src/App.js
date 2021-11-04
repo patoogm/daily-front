3fd394330f5511a57803a8e9831274b7f6b19336
Martin  Frias Silva <Martin@macbook-martin.home>
2021-11-04 15:05:12 -0300

    modificando grilla

--- src/App.js
+++ src/App.js
@@ -6,11 +6,8 @@ import Footer from "./components/footer/Footer";
 import Carrousel from './components/carrousel/Carrousel';
 import Title1 from './components/titles/Title1';
 import Card1 from './components/cards/Card1';
-import Title2 from './components/titles/Title2';
-import Card2 from './components/cards/Card2';
 import Advertising from './components/advertising/Advertising';
-import Title3 from './components/titles/Title3';
-import Card3 from './components/cards/Card3';
+
 
 function App() {
   return (
@@ -20,11 +17,11 @@ function App() {
       <Carrousel />
       <Title1 />
       <Card1 />
-      <Title2 />
-      <Card2 />
+      <Title1 />
+      <Card1 />
       <Advertising />
-      <Title3 />
-      <Card3 />
+      <Title1 />
+      <Card1 />
       <Footer />
     </div>
   );
