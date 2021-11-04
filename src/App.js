043d0a049d30c6e0e1fb3feabaf58b1df777a4e4
Martin  Frias Silva <Martin@macbook-martin.home>
2021-11-04 15:26:55 -0300

    modificacion titulos

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import '../src/components/header/header.css'
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Carrousel from './components/carrousel/Carrousel';
-import Title1 from './components/titles/Title1';
 import Card1 from './components/cards/Card1';
 import Advertising from './components/advertising/Advertising';
 
@@ -15,12 +14,12 @@ function App() {
       <Header />
       <Navbar />
       <Carrousel />
-      <Title1 />
+      <h2 className="p-4">Ultimas Noticias </h2>
       <Card1 />
-      <Title1 />
+      <h2 className="p-4">Deportes </h2>
       <Card1 />
       <Advertising />
-      <Title1 />
+      <h2 className="p-4">Politica </h2>
       <Card1 />
       <Footer />
     </div>
--- src/components/titles/Title1.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function App() {
-  return (
-    <div className="text-nowrap bd-highlight p-4" style={{width: "8rem"}}>
-      Ultimas Noticas
-    </div>
-  );
-}
